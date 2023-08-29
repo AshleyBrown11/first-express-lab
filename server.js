df350876a51782c82c4d8b04fe7ab9d11b711fee
Ashley <ajbrown_88@yahoo.com>
2023-08-29 16:20:48 -0400

    "Magic 8 Ball express application created."

--- server.js
+++ server.js
@@ -40,4 +40,12 @@ app.get('/tip/:total/:tipPercentage', (req, res) => {
     res.send(`<h1>Your tip amount is: ${tipAmount}</h1>`);
   });
   
- 
+ // Magic 8 Ball
+ app.get('/magic', (req, res) => {
+    // Create response array
+    const responses = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now", "Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"]
+    // Returns a number
+    // const randomResponse = Math.random(req.params.response);
+    res.send(`<h1> Will I be a Millionaire? <br> Magic Eight Ball Says: "${randomResponse}"</h1>`);
+  });
+  
